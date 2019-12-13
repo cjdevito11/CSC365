054e4b233e84e1ab58445b3fbff126d7101ee849
Christian DeVito <cdevito@oswego.edu>
2019-12-13 06:36:14 -0500

    Simple Edge Object

---
+++ src/a1/a3/Edge.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package a1.a3;
+
+/**
+ *
+ * @author CJ
+ */
+public class Edge {
+    
+    public Node src;
+    public Node dest;
+    
+    int srcIndex;
+    int destIndex;
+    
+    double weight;
+
+    public Edge(int srcIndex, int destIndex, double weight){
+        this.srcIndex = srcIndex;
+        this.destIndex = destIndex;
+        this.weight = weight;
+    }
+    
+    public int getSrcIndex(){ return srcIndex; }
+    public int getDestIndex() { return destIndex; }
+    public double getWeight() { return weight; }
+    
+    public int getConnectIndex(int index) {
+        if (this.srcIndex == index){
+            return this.destIndex;                  // IF SRC GO TO NEXT CONNECT
+        } else {
+            return this.srcIndex;
+        }
+    }
+    
+}
