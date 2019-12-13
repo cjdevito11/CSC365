8875628dfce580414ae68a77801f77c3cf391867
Christian DeVito <cdevito@oswego.edu>
2019-12-13 11:20:41 -0500

    add edges

--- src/a1/a3/Node.java
+++ src/a1/a3/Node.java
@@ -6,7 +6,8 @@ import java.util.HashSet;
 import java.util.NoSuchElementException;
 
 public class Node {
-    String id;
+    
+    public String url;
    // HashSet<Edge> edges = new HashSet<>();
 
     double totalDistance = Integer.MAX_VALUE;
@@ -14,62 +15,22 @@ public class Node {
     int index;
     double cost;
 
-    //Node(String id) { this.id = id;}    // Construct
+    public Node() {}    // Construct
+    public Node(String id) { this.url = url;}    // Construct
 
     private ArrayList<Edge> edges = new ArrayList<>(); // now we must create edges
     
     public double getTotalDistance() { return totalDistance; }
     public ArrayList<Edge> getEdges() { return edges; } 
+    public int getEdgeSize(){ return edges.size(); }
     public boolean isVisited() { return visited; } 
         
     public void setTotalDistance(double totalDistance) {this.totalDistance = totalDistance;}  
     public void setVisited(boolean visited) { this.visited = visited; } 
     public void setEdges(ArrayList<Edge> edges) { this.edges = edges; }
+    public void setUrl(String url){this.url = url;}
+    public void addEdge(Edge edge){this.edges.add(edge);}
     
 }
     
-    
-    /*
-
-    void addEdge(Node dest, double w) {     //Add edge w/ Weight = w 
-            Edge edge = new Edge(this,dest,w);
-            edge.weight = w;
-
-            edge.src = this;                //connect this w/ next node
-            edge.dest = dest;
-            edges.add(edge);
-    }
-
-    public double findEdge(Node dst) {             // find & return edges weight
-        for (Edge e : edges) {
-            if (e.dest.equals(dst)) {
-                return e.weight;
-            }
-        }
-        throw new NoSuchElementException();
-    }
-
-    private void writeObject(java.io.ObjectOutputStream out) throws IOException {
-            out.writeObject(id);
-    }
-
-    private void readObject(java.io.ObjectInputStream in) throws IOException, ClassNotFoundException {
-            edges = new HashSet<>();
-            children = new HashSet<>();
-            id = (String) in.readObject();
-    }
-    
-    @Override
-    public boolean equals(Object obj) {
-            return obj instanceof Node && id.equals(((Node) obj).id);
-    }
-
-    @Override
-    public String toString() { return id; }
-
-    @Override
-    public int hashCode() { return id.hashCode(); }
-
-*/
-
-
+  
